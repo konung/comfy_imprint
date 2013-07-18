653280e1119eb7241aba653f36baac3798625368
Nick Gorbikoff <nick.gorbikoff@gmail.com>
2013-07-18 15:55:02 -0500

    fixing security hole!

--- app/controllers/comfy_imprint/contacts_controller.rb
+++ app/controllers/comfy_imprint/contacts_controller.rb
@@ -7,9 +7,6 @@ module ComfyImprint
     def index
       redirect_to root_path
     end
-    # GET /contacts/1
-    def show
-    end
 
     # GET /contacts/new
     def new
@@ -23,7 +20,7 @@ module ComfyImprint
         # Contact Attempt passed validation so it's safe to mail it.
         # In the future I may want to impleme nt some kind of delayed_job or redis hook here
         ContactMailer.contact_email(@contact).deliver
-        redirect_to @contact#, notice: 'Contact was successfully created.'
+        redirect_to :thank_you #, notice: 'Contact was successfully created.'
       else
         render action: :new
       end
@@ -33,6 +30,9 @@ module ComfyImprint
     def destroy
       @contact.destroy
       redirect_to contacts_url#, notice: 'Contact was successfully destroyed.'
+    end
+
+    def thank_you
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ComfyImprint::Engine.routes.draw do
-  resources :contacts , only: [:index, :create, :new, :show]
+  resources :contacts , only: [:index, :create, :new]
+  match 'thank-you', to: 'contacts#thank_you'
+  match 'thank_you', to: 'contacts#thank_you'
   root to: "contacts#new"
 end
